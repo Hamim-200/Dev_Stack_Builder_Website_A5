2a0f03d63bdfa52999eaacc197d883c75c4774c0
Hamim Ahmed <hamim.ahmed541@gmail.com>
2026-09-13 13:48:26 +0600

    first commit

---
+++ dev-stack-builder/src/App.tsx
@@ -0,0 +1,17 @@
+import Nav from "./Nav"
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h2 className="text-5xl text-amber-600">Hello REACT</h2>
+        <button className="btn btn-warning">Warning</button>
+
+      <Nav></Nav>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ dev-stack-builder/src/Nav.tsx
@@ -0,0 +1,9 @@
+const Nav = () => {
+    return (
+        <div>
+            <h1>This is navbar</h1>
+        </div>
+    );
+};
+
+export default Nav;
